4a5a64bd577687f0cfa7f7f08bfdbd55363c7a57
JRESTREM <jrestrem@proteccion.com.co>
2021-04-20 16:05:41 -0500

    Se agrega base de proyecto

---
+++ src/main/java/co/com/mercado/libre/mutant/detector/MutantDetectorApplication.java
@@ -0,0 +1,13 @@
+package co.com.mercado.libre.mutant.detector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MutantDetectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MutantDetectorApplication.class, args);
+	}
+
+}
