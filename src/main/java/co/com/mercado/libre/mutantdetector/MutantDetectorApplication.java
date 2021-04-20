eea4c12a58a5256ba03f81f41f36f80b8ea94c9c
JRESTREM <jrestrem@proteccion.com.co>
2021-04-20 18:11:46 -0500

    Se configuran inciales de proyecto

--- src/main/java/co/com/mercado/libre/mutantdetector/MutantDetectorApplication.java
+++ src/main/java/co/com/mercado/libre/mutantdetector/MutantDetectorApplication.java
@@ -1,4 +1,4 @@
-package co.com.mercado.libre.mutant.detector;
+package co.com.mercado.libre.mutantdetector;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
